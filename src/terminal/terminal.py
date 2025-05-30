ecec5fa3442256196409784157e2711c62d08963
JasonAlbertEinstien <j2004nol@gmail.com>
2025-05-31 00:20:16 +0800

    terminal

---
+++ src/terminal/terminal.py
@@ -0,0 +1,7 @@
+import subprocess
+
+class Terminal:
+    def __init__(self):
+        pass
+    def ExecuteCommand(self, command:str):
+        return subprocess.run(command , shell=True , check=True)
