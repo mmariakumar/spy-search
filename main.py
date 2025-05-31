fd74488d0919d3748650354102e706d9d64f01a8
JasonAlbertEinstien <j2004nol@gmail.com>
2025-05-31 14:38:55 +0800

    add system prompt

--- main.py
+++
@@ -1,4 +0,0 @@
-from src.model.gemini import Gemini
-
-g = Gemini("gemini-2.5-flash")
-print(g.completion("what is ai"))
--- src/model/gemini.py
+++ src/model/gemini.py
@@ -1,4 +1,6 @@
-from google import genai #use openai seems better ?
+from google import genai 
+from google.genai import types 
+
 import os 
 from dotenv import load_dotenv
 
@@ -10,10 +12,19 @@ class Gemini(Model):
         self.api_key = os.getenv("GEMINI_API")
         self.model = model
         self.client = genai.Client(api_key=self.api_key)
+        self.message = self.client.chats.create(model=model)
 
     def completion(self , query:str):
-        # TODO: current generae content is not work
-        res = self.client.models.generate_content(
-            model="gemini-2.0-flash" , contents=query
+        res = self.message.send_message(query)
+        return res.text
+
+    def reset(self):
+        """
+            Reset chat message
+        """
+        self.message = self.client.chats.create(model=Model)
+
+    def add_system_instruction(self, instruction:str):
+        self.message.send_message(config=types.GenerateContentConfig(
+            system_instruction=instruction)
         )
-        return res 
