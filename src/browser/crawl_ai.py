5c9f860d26e357494b92990affd3c78b1c02eadc
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-03 15:31:21 +0800

    init

--- src/browser/crawl_ai.py
+++ src/browser/crawl_ai.py
@@ -1,36 +1,57 @@
 # we may use crew_ai write some api for it
-
+from crawl4ai import AsyncWebCrawler , BrowserConfig , CrawlerRunConfig , CacheMode
 class Crawl:
     """
-    Crawl4ai should be used to support browser.py
+    Crawl4ai should be used to support browser.py [Maybe we don't even need browser.py]
     with browser.py search some well known website for example google arxiv google scholar
     or some user self defined website --> seach all link
 
     --> then use crawl4ai to help for search
 
+    All api is expected to be sync and use crawl4ai
     Expected API list
         get_links: given a url which is the result from a search website like google return the result list of that page
         get_images: get all images from the webpage
         get_content: get relevant content to a markdown
-
-        
     """
+    def __init__(self, model , db=None , url_search = None):
+        self.model = model
+        self.crawler = AsyncWebCrawler()
+        self.db = [] if db == None else db
 
-    def __init__(self, model):
-        pass
+        """
+            The url_list is the list that we hope to search in the next ste 
+            The url_search list is list of well known website that we want to search with 
+        """
+        self.url_list = []  
+        self.url_search =[]
+
+        self.broswer_conf = BrowserConfig()
+        self.run_conf = CrawlerRunConfig()
+
+    async def start_crawler(self):
+        await self.crawler.start()
+
+    async def close_crawler(self):
+        await self.crawler.close()
+
+
+    async def get_url_llm(self):
+        """
+            Get url from a website with the help of llm
+        """
+        pass 
+
+
+    def screen_shot(self):
+        pass 
+
+    def search_content(self):
+        pass 
+
+    def run(self):
+        pass 
+
 
 if __name__ == "__main__":
-    import asyncio
-    from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
-
-    async def main():
-        browser_conf = BrowserConfig(headless=True)  # or False to see the browser
-        run_conf = CrawlerRunConfig(
-            cache_mode=CacheMode.BYPASS,
-        )
-
-        async with AsyncWebCrawler(config=browser_conf) as crawler:
-            result = await crawler.arun(url="https://google.com", config=run_conf)
-            print(result.markdown)
-
-    asyncio.run(main())
+    pass
