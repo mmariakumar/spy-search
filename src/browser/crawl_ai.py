bda5f5c8329279d422b19687f00c523ecc9c25e3
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-03 11:18:30 +0800

    workflow comment

--- src/browser/crawl_ai.py
+++ src/browser/crawl_ai.py
@@ -1,6 +1,5 @@
 # we may use crew_ai write some api for it
 
-
 class Crawl:
     """
     Crawl4ai should be used to support browser.py
@@ -8,11 +7,17 @@ class Crawl:
     or some user self defined website --> seach all link
 
     --> then use crawl4ai to help for search
+
+    Expected API list
+        get_links: given a url which is the result from a search website like google return the result list of that page
+        get_images: get all images from the webpage
+        get_content: get relevant content to a markdown
+
+        
     """
 
     def __init__(self, model):
         pass
-
 
 if __name__ == "__main__":
     import asyncio
@@ -20,7 +25,9 @@ if __name__ == "__main__":
 
     async def main():
         browser_conf = BrowserConfig(headless=True)  # or False to see the browser
-        run_conf = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+        run_conf = CrawlerRunConfig(
+            cache_mode=CacheMode.BYPASS,
+        )
 
         async with AsyncWebCrawler(config=browser_conf) as crawler:
             result = await crawler.arun(url="https://google.com", config=run_conf)
