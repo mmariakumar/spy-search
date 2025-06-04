3e7cb838c85d83c2959489126142877eb4c21827
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-04 17:37:30 +0800

    planner

--- src/agent/search.py
+++ src/agent/search.py
@@ -3,14 +3,41 @@ from ..model import model
 
 
 class Search_agent(Agent):
-    def __init__(self, model: model):
+    def __init__(self, model: model , k:int = 10):
+        """
+            take some default URL for search  
+            k: number of steps
+        """
         self.model = model
+
+        self.search_web = [
+            "https://google.com",
+            "https://arxiv.com",
+            "https://news.google.com",
+            "https://scholar.google.com"
+        ]
+
+        self.planner = []
+        self.step = 10
 
     def run(self, task , data) -> str:
         """
             Search function need to user the brower methods to search relevant contents 
             - note that search agent should have it's own planner to plan search with what links
-            - 
+            
+            Steps:
+                1. Generate a to do list
+                2. For each task
+                    read current short summary to plan the searching key word 
+                    selecte the search_web
+                    allow one step depth search [hyper paramerter ?]
+                    script the content if irrelevant --> ignore
+                    if relevant --> self to db 
+                    generate long short summary 
+                3. Return two things
+                    for data we want to reutrn the long summary 
+                    for response we just need to response "FINISHED" 
+
         """
         return {
             "agent":"TERMINATE"
@@ -21,3 +48,12 @@ class Search_agent(Agent):
 
     def get_recv_format(self):
         pass
+
+
+    def _plan(self):
+        """
+            Searcher planner 
+        """
+        pass 
+
+
