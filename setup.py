907561df6ab4e7347b5c1ffd9b1170de1acf7612
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-08 12:13:11 +0800

    open ai support

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     DEEPSEEK_API=<YOUR-DEEPSEEK-API>
     GEMINI_API=<YOU-GEMINI-API>
     XAI_API_KEY=<XAI API KEY>
+    OPENAI_API_KEY=<YOUR OPENAI API KEY>
     """
     create_env_file(file_path, env_content)
     print(f".env file created at {os.path.abspath(file_path)}")
