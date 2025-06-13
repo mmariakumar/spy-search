b99e3f04f8534f4c224ffe6be8d06f1130dfd19e
lokxii <9tin9tin9@gmail.com>
2025-06-13 18:16:02 +0800

    Fix .env formatting

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ def create_json_file(path, content):
 
 if __name__ == "__main__":
     file_path = ".env"
-    env_content = """
-    DEEPSEEK_API=<YOUR-DEEPSEEK-API>
-    GEMINI_API=<YOU-GEMINI-API>
-    XAI_API_KEY=<XAI API KEY>
-    OPENAI_API_KEY=<YOUR OPENAI API KEY>
-    """
+    env_content = """\
+DEEPSEEK_API=<YOUR-DEEPSEEK-API>
+GEMINI_API=<YOU-GEMINI-API>
+XAI_API_KEY=<XAI API KEY>
+OPENAI_API_KEY=<YOUR OPENAI API KEY>
+"""
     create_env_file(file_path, env_content)
     print(f".env file created at {os.path.abspath(file_path)}")
 
